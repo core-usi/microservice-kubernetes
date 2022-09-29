9368aac20a7c8335504833c28a0c63c6ce4f3d45
ramettan <48575467+ramettan@users.noreply.github.com>
2022-09-29 13:57:45 +0530

    Update docker-build.sh

--- microservice-kubernetes-demo/docker-build.sh
+++ microservice-kubernetes-demo/docker-build.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
-if [ -z "$DOCKER_ACCOUNT" ]; then
-    DOCKER_ACCOUNT=ewolff
-fi;
+
 docker build --tag=microservice-kubernetes-demo-apache apache
-docker tag microservice-kubernetes-demo-apache $DOCKER_ACCOUNT/microservice-kubernetes-demo-apache:latest
-docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-apache
+docker tag microservice-kubernetes-demo-apache $1/microservice-kubernetes-demo-apache:latest
+docker push $1/microservice-kubernetes-demo-apache
 
 docker build --tag=microservice-kubernetes-demo-catalog microservice-kubernetes-demo-catalog
-docker tag microservice-kubernetes-demo-catalog $DOCKER_ACCOUNT/microservice-kubernetes-demo-catalog:latest
-docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-catalog
+docker tag microservice-kubernetes-demo-catalog $1/microservice-kubernetes-demo-catalog:latest
+docker push $1/microservice-kubernetes-demo-catalog
 
 docker build --tag=microservice-kubernetes-demo-customer microservice-kubernetes-demo-customer
-docker tag microservice-kubernetes-demo-customer $DOCKER_ACCOUNT/microservice-kubernetes-demo-customer:latest
-docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-customer
+docker tag microservice-kubernetes-demo-customer $1/microservice-kubernetes-demo-customer:latest
+docker push $1/microservice-kubernetes-demo-customer
 
 docker build --tag=microservice-kubernetes-demo-order microservice-kubernetes-demo-order
-docker tag microservice-kubernetes-demo-order $DOCKER_ACCOUNT/microservice-kubernetes-demo-order:latest
-docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-order
+docker tag microservice-kubernetes-demo-order $1/microservice-kubernetes-demo-order:latest
+docker push $1/microservice-kubernetes-demo-order
